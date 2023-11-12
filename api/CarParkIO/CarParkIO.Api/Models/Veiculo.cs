469fae5eef8080abafea2a00ac1134c76ecadf52
Victor Fernando Ferreira Gomes <victorgbpk@gmail.com>
2023-11-12 10:42:08 -0300

    Ajustes e models

---
+++ api/CarParkIO/CarParkIO.Api/Models/Veiculo.cs
@@ -0,0 +1,15 @@
+﻿namespace CarParkIO.Api.Models
+{
+    public class Veiculo
+    {
+        public Veiculo(string placa)
+        {
+            Id = Guid.NewGuid();
+            Placa = placa;
+        }
+
+        public Guid Id { get; private set; }
+
+        public string Placa { get; private set; }
+    }
+}
--- api/CarParkIO/CarParkIO.Api/Program.cs
+++ api/CarParkIO/CarParkIO.Api/Program.cs
@@ -1,9 +1,24 @@
-using CarParkIO.Api;
+using CarParkIO.Api.Models;
+using Microsoft.AspNetCore.Authorization;
+using System.Runtime.ConstrainedExecution;
+
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      policy =>
+                      {
+                          policy.WithOrigins("http://localhost:5173");
+                      });
+});
+
+builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
@@ -16,12 +31,21 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 var estacionamento = new Estacionamento(0, 0);
+estacionamento.AdicionarVeiculo(new Veiculo("FXX2321"));
 
-app.MapGet("/listar-veiculos", () =>
+app.MapGet("/listar-veiculos", [AllowAnonymous] () =>
 {
     return estacionamento.ListarVeiculos().ToArray();
 })
-.WithName("GetWeatherForecast")
+.WithName("CarParkIO")
 .WithOpenApi();
+
+app.UseCors(builder => builder
+    .AllowAnyOrigin()
+    .AllowAnyMethod()
+    .AllowAnyHeader()
+);
+
+app.UseAuthorization();
 
 app.Run();
